96074ea05bddd015f222d59c7d4e77a841af4977
lizhongyuan <fenixlee@163.com>
2021-03-31 23:31:01 +0800

    remove the cpp file in binary_tree

--- sort/merge_sort.cpp
+++ sort/merge_sort.cpp
@@ -5,6 +5,7 @@
 #include "util.h"
 
 
+<<<<<<< 9e71d6d80513a1d56b8c861d32bb2365359fe174
 void merge(int* sort_array, int* unsort_array, int left, int mid, int right) {
   for (int k = left; k <= right; k++) {
     unsort_array[k] = sort_array[k];
@@ -32,6 +33,35 @@ void merge(int* sort_array, int* unsort_array, int left, int mid, int right) {
 
   while (index2 <= right) {
     sort_array[index++] = unsort_array[index2++];
+=======
+void merge(int* arr1, int* arr2, int left, int mid, int right) {
+  for (int k = left; k <= right; k++) {
+    arr2[k] = arr1[k];
+  }
+
+  int s1 = left;
+  int s2 = mid + 1;
+  int t = left;
+
+  while (s1 <= mid && s2 <= right) {
+    if (arr2[s1] <= arr2[s2]) {
+      arr1[t] = arr2[s1];
+      t++;
+      s1++;
+    } else {
+      arr1[t] = arr2[s2];
+      t++;
+      s2++;
+    }
+  }
+
+  while (s1 <= mid) {
+    arr1[t++] = arr2[s1++];
+  }
+
+  while (s2 <= right) {
+    arr1[t++] = arr2[s2++];
+>>>>>>> remove the cpp file in binary_tree
   }
 }
 
