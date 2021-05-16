1431f3a06426d1e0111f4f5a30e5002ce0043ed5
lizhongyuan <fenixlee@163.com>
2021-05-16 18:14:17 +0800

    add set and dictionary files

---
+++ set/bit_set.h
@@ -0,0 +1,8 @@
+//
+// Created by cyberdash@163.com(抖音: cyberdash_yuan) on 2021/5/16.
+//
+
+#ifndef CYBER_DASH_BIT_SET_H
+#define CYBER_DASH_BIT_SET_H
+
+#endif // CYBER_DASH_BIT_SET_H
---
+++ set/set.h
@@ -0,0 +1,15 @@
+//
+// Created by cyberdash@163.com(抖音: cyberdash_yuan) on 2021/5/16.
+//
+
+#ifndef CYBER_DASH_SET_H
+#define CYBER_DASH_SET_H
+
+
+class Set {
+public:
+  virtual Set() = 0;
+  virtual MakeEmpty() = 0;
+};
+
+#endif // CYBER_DASH_SET_H
