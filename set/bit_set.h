3f6d6362fd6a1c3dde3de7d1e0aa8f52dc84d988
Allen Yu <allenyu@AllendeMacBook-Air.local>
2021-05-26 00:22:34 +0800

    add set and bit_set

--- set/bit_set.h
+++ set/bit_set.h
@@ -5,4 +5,39 @@
 #ifndef CYBER_DASH_BIT_SET_H
 #define CYBER_DASH_BIT_SET_H
 
+#include<assert.h>
+#include<iostream>
+// enum bool {false,true};
+const int DefaultSize = 50;
+template<typename T>
+class bitSet{
+public:
+    bitSet(int sz = DefaultSize);
+    bitSet(const bitSet<T> &R);
+    ~bitSet(){delete []bitVector;};
+    void makeEmpty()
+    {
+        for (int i = 0; i < vectorSize; i++)
+        {
+            bitVector[i]=0;
+        }
+    }
+    bool getMember(const T &x);
+    void putMember(const T &x, int v);
+    bool addMember(const T &x);
+    bool delMember(const T &x);
+    bitSet<T>& operator = (const bitSet<T> &R);
+    bitSet<T> operator + (const bitSet<T> &R);
+    bitSet<T> operator * (const bitSet<T> &R);
+    bitSet<T> operator - (const bitSet<T> &R);
+    bool Contains(const T x);
+    bool subSet(bitSet<T> &R);
+    bool operator == (bitSet<T> &R);
+    friend istream& operator >> (istream &in, bitSet<T> &R);
+    friend ostream& operator << (ostream &out, bitSet<T> &R);
+private:
+    int setSize;
+    int vectorSize;
+    unsigned short *bitVector;
+};
 #endif // CYBER_DASH_BIT_SET_H
--- set/set.h
+++ set/set.h
@@ -5,11 +5,19 @@
 #ifndef CYBER_DASH_SET_H
 #define CYBER_DASH_SET_H
 
-
+template<class T>
 class Set {
 public:
   virtual Set() = 0;
   virtual MakeEmpty() = 0;
+  virtual bool addMember(const T x)=0;
+  virtual bool delMember(const T x)=0;
+  virtual Set<T> intersectWith(const Set<T>& R)=0;
+  virtual Set<T> unionWith(const Set<T>& R)=0;
+  virtual Set<T> differenceFrom(const Set<T>& R)=0;
+  virtual bool Contains(const T x)=0;
+  virtual bool subSet(const Set<T>& R)=0;
+  virtual bool operator == (const Set<T>& R)=0;
 };
 
 #endif // CYBER_DASH_SET_H
