adc9260a84a511d2ba4da10f7aeb467343e8d722
Y_Dash <cyberdash@163.com>
2021-07-19 16:05:30 +0000

    Remove search/AVLTree/src

--- search/AVLTree/src/test.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by cyberdash@163.com on 2021/7/3.
-//
-
-#include "test.h"
--- search/AVLTree/src/test.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by cyberdash@163.com on 2021/7/3.
-//
-
-#ifndef MAIN_TEST_H
-#define MAIN_TEST_H
-
-#endif //MAIN_TEST_H
