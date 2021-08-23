91f9ed8c019dd316987454244e43687bdf19ec6c
Lei Yu <Lei.Yu@MediaKind.com>
2021-08-23 18:04:05 +0800

    add doxygen note

--- array/src/sparse_matrix.h
+++ array/src/sparse_matrix.h
@@ -1,6 +1,13 @@
-//
-// Created by cyberdash@163.com(抖音id:cyberdash_yuan) on 2021/5/16.
-//
+/**
+ * @file sparse_matrix.h
+ * @author cyberdash@163.com(抖音id:cyberdash_yuan)
+ * @brief 稀疏矩阵 
+ * @version 0.1
+ * @date 2021-05-13
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 #ifndef CYBER_DASH_TRI_TUPLE_H
 #define CYBER_DASH_TRI_TUPLE_H
@@ -11,10 +18,19 @@ struct TriTuple {
 
 };
 
-
+/**
+ * @brief 构造稀疏矩阵
+ * 
+ * @tparam T 存储的值
+ */
 template<class T>
 class SparseMatrix {
 public:
+/**
+ * @brief 构建一个新的稀疏矩阵
+ * 
+ * @param max_size 矩阵最大容量
+ */
   SparseMatrix(int max_size);
   SparseMatrix(SparseMatrix<T>& sparse_matrix);
 
@@ -27,4 +43,8 @@ private:
 };
 
 
-#endif // CYBER_DASH_TRI_TUPLE_H
+#endif
+/**
+ * @brief  CYBER_DASH_TRI_TUPLE_H
+ * 
+ */
