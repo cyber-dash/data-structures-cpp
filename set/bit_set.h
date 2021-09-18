c7293bf05afe36c3ce7464946b6797464fd4d714
lizhongyuan <fenixlee@163.com>
2021-09-18 18:02:47 +0800

    add code in set

--- set/bit_set.h
+++ set/bit_set.h
@@ -5,60 +5,92 @@
 #ifndef CYBER_DASH_BIT_SET_H
 #define CYBER_DASH_BIT_SET_H
 
-// #include<assert.h>
-#include<iostream>
-
+#include "set.h"
+#include <iostream>
 
 // enum bool {false,true};
-const int DefaultSize = 50;
+// const int DefaultSize = 50;
 
 
 template<typename T>
 class BitSet: public Set<T> {
 public:
 
-  BitSet(int size = DefaultSize);
+  BitSet(int size);
   BitSet(const BitSet<T>& bit_set);
-  ~BitSet(){ delete[] bit_vector_; };
-  void makeEmpty();
-  bool GetMember(const T &x);
-  void PutMember(const T &x, int v);
-  bool AddMember(const T &x);
-  bool DelMember(const T &x);
-  BitSet<T>& operator = (const BitSet<T> &R);
-  BitSet<T> operator + (const BitSet<T> &R);
-  BitSet<T> operator * (const BitSet<T> &R);
-  BitSet<T> operator - (const BitSet<T> &R);
-  bool Contains(const T x);
+
+  // 析构函数
+  ~BitSet() { delete[] this->bit_vector_; };
+
+  // 置空集合
+  void MakeEmpty();
+
+  int GetSetSize();
+  void SetSetSize(int set_size);
+
+  int GetVectorSize();
+  void SetVectorSize(int vector_size);
+
+  unsigned short GetMember(const T& x);
+  void PutMember(const T& x, int v);
+  bool AddMember(const T& x);
+  bool DelMember(const T& x);
+  bool Contains(T x);
   bool SubSet(BitSet<T>& bit_set);
+
+  BitSet<T>& operator = (const BitSet<T>& bit_set);
+  BitSet<T>& operator + (const BitSet<T>& bit_set);
+  BitSet<T>& operator * (const BitSet<T>& bit_set);
+  BitSet<T>& operator - (const BitSet<T>& bit_set);
   bool operator == (BitSet<T>& bit_set);
+
   friend istream& operator >> (istream &in, BitSet<T>& bit_set);
   friend ostream& operator << (ostream &out, BitSet<T>& bit_set);
 
 private:
-  int set_size_;
-  int vector_size_;
-  unsigned short* bit_vector_;
+  int set_size_; // 集合大小
+  int vector_size_; // 位数组大小
+  unsigned short* bit_vector_; // 存储集合元素的位数组
 };
 
 
 template<typename T>
-void BitSet<T>::makeEmpty() {
-  for (int i = 0; i < vector_size_; i++)
-  {
-    bit_vector_[i]=0;
+void BitSet<T>::MakeEmpty() {
+  for (int i = 0; i < this->vector_size_; i++) {
+    this->bit_vector_[i]=0;
   }
 }
 
-template<typename T>
-BitSet<T>::BitSet(int size) {
 
+/**
+ * @brief 构造函数
+ * @tparam T 模板数据类型
+ * @param size 集合大小
+ */
+template<typename T>
+BitSet<T>::BitSet(int size): set_size_(size) {
+  this->vector_size_ = (this->set_size_ + 15) >> 4;
+  this->bit_vector_ = new unsigned short [this->vector_size_];
+  /* error handler */
+
+  // init
+  for (int i = 0; i < this->vector_size_; i++) {
+    this->bit_vector_[i] = 0;
+  }
 }
 
-template<typename T>
-BitSet<T>::BitSet(const BitSet<T> &bit_set) {
 
+/**
+ * @brief 构造函数
+ * @tparam T 模板数据类型
+ * @param bit_set (源)集合
+ */
+template<typename T>
+BitSet<T>::BitSet(const BitSet<T>& src_bit_set) {
+  this->set_size_ = src_bit_set.set_size_;
+  this->vector_size_ = src_bit_set.
 }
+
 
 template<typename T>
 bool BitSet<T>::GetMember(const T &x) {
@@ -113,6 +145,10 @@ bool BitSet<T>::SubSet(BitSet<T> &bit_set) {
 template<typename T>
 bool BitSet<T>::operator==(BitSet<T> &bit_set) {
   return false;
+}
+
+istream &operator>>(istream &in, BitSet<T> &bit_set) {
+  return <#initializer#>;
 }
 
 ostream &operator<<(ostream &out, BitSet<T> &bit_set) {
--- set/set.h
+++ set/set.h
@@ -5,19 +5,40 @@
 #ifndef CYBER_DASH_SET_H
 #define CYBER_DASH_SET_H
 
+
 template<class T>
 class Set {
 public:
+
+  // 构造函数
   virtual Set() = 0;
+
+  // 置空集合
   virtual MakeEmpty() = 0;
-  virtual bool AddMember(T x)=0;
-  virtual bool DelMember(T x)=0;
-  virtual Set<T> IntersectWith(const Set<T>& R)=0;
-  virtual Set<T> UnionWith(const Set<T>& R)=0;
-  virtual Set<T> DifferenceFrom(const Set<T>& R)=0;
-  virtual bool Contains(T x)=0;
-  virtual bool SubSet(const Set<T>& R)=0;
-  virtual bool operator == (const Set<T>& R)=0;
+
+  // 加入新成员
+  virtual bool AddMember(T x) = 0;
+
+  // 删除成员
+  virtual bool DelMember(T x) = 0;
+
+  // 集合的交运算
+  virtual Set<T> IntersectWith(const Set<T>& set) = 0;
+
+  // 集合的并运算
+  virtual Set<T> UnionWith(const Set<T>& set) = 0;
+
+  // 集合的差运算
+  virtual Set<T> DifferenceFrom(const Set<T>& set) = 0;
+
+  // 判断是否是集合的成员
+  virtual bool Contains(T x) = 0;
+
+  // 判断是否是集合的子集
+  virtual bool SubSet(const Set<T>& set) = 0;
+
+  // 判断集合是否相等
+  virtual bool operator == (const Set<T>& set) = 0;
 };
 
 
