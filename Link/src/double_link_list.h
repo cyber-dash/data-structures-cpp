003ea1b7562b320121d080be080b39de1b10d372
lizhongyuan <fenixlee@163.com>
2021-09-27 19:11:31 +0800

    add link list doxygen conf file

--- Link/src/double_link_list.h
+++ Link/src/double_link_list.h
@@ -1,13 +1,31 @@
-//
-// Created by cyberdash@163.com(抖音: cyberdash_yuan) on 2020/7/28.
-//
+/*!
+ * @file link_list.h
+ * @author CyberDash计算机考研, cyberdash@163.com(抖音id:cyberdash_yuan)
+ * @brief 双向链表
+ * @version 0.2.1
+ * @date 2020-07-28
+ * @copyright Copyright (c) 2021
+ * **CyberDash计算机考研**
+ */
 
 #ifndef CYBER_DASH_DOUBLE_LINK_LIST_H
 #define CYBER_DASH_DOUBLE_LINK_LIST_H
 
 
 #include <iostream>
-#include "double_node.h"
+
+
+template <class T>
+struct DoubleLinkNode {
+
+  explicit DoubleLinkNode(DoubleLinkNode<T>* next = NULL, DoubleLinkNode<T>* prev = NULL): prev_(prev), next_(next) {}
+  explicit DoubleLinkNode(const T& data, DoubleLinkNode<T>* next = NULL, DoubleLinkNode<T>* prev = NULL):
+      data_(data), prev_(prev), next_(next) {}
+
+  T data_;
+  DoubleLinkNode<T>* next_;
+  DoubleLinkNode<T>* prev_;
+};
 
 
 template<class T>
@@ -18,9 +36,9 @@ public:
   DoubleLinkList(T data);
   ~DoubleLinkList();
 
-  int Length() const { return length_; }
+  int Length() const { return this->length_; }
   bool IsEmpty() const;
-  DoubleLinkNode<T>* GetHead() const { return head_ptr_; }
+  DoubleLinkNode<T>* GetHead() const { return this->head_ptr_; }
   DoubleLinkNode<T>* Search(const T& data);
   DoubleLinkNode<T>* Locate(int pos, int direction);
   bool Insert(int pos, const T& data, int direction);
--- Link/src/link_list.h
+++ Link/src/link_list.h
@@ -1,15 +1,11 @@
-//
-// Created by cyberdash@163.com(抖音: cyberdash_yuan) on 2020/6/6.
-//
 /*!
  * @file link_list.h
  * @author CyberDash计算机考研, cyberdash@163.com(抖音id:cyberdash_yuan)
- * @brief 稀疏矩阵
+ * @brief 单向链表
  * @version 0.2.1
  * @date 2020-06-06
- *
  * @copyright Copyright (c) 2021
- *  CyberDash计算机考研
+ * **CyberDash计算机考研**
  */
 
 #ifndef CYBER_DASH_LINK_LIST_H
@@ -17,7 +13,28 @@
 
 
 #include <iostream>
-#include "node.h"
+#include <cstddef>
+
+
+using namespace std;
+
+
+/*!
+ * @class 链表结点模板类
+ * @tparam T 类型模板参数
+ */
+template <class T>
+struct LinkNode {
+  /*! @brief 构造函数(结点指针) */
+  explicit LinkNode(LinkNode<T>* ptr = NULL) { this->link_ = ptr; }
+  explicit LinkNode(const T& data, LinkNode<T> *ptr = NULL) {
+    this->data_ = data;
+    this->link_ = ptr;
+  }
+
+  T data_;
+  LinkNode<T>* link_;
+};
 
 
 template<class T>
@@ -25,9 +42,12 @@ class LinkList {
 
 public:
 
+  // 构造函数(无参数)
   LinkList();
+  // 构造函数(参数为头结点的数据项)
   LinkList(const T& data);
-  LinkList(LinkNode<T>*& node);
+  // 构造函数(参数为节点指针引用)
+  LinkList(LinkNode<T>* node);
   LinkList(LinkList<T>& link_list);
   ~LinkList();
 
@@ -38,7 +58,7 @@ public:
   LinkNode<T>* Locate(int pos);
   bool GetData(int pos, T& data) const;
   bool SetData(int pos, T& data);
-  bool Insert(int pos, T& data);
+  bool Insert(int pos, const T& data);
   bool Remove(int pos, T& data);
   bool IsEmpty() const;
   bool IsFull() const;
@@ -49,8 +69,246 @@ public:
 private:
   LinkNode<T>* head_ptr_;
   int length_;
-  int size_;
+  // int size_;
 };
+
+
+template<class T>
+LinkList<T>::LinkList() {
+  this->head_ptr_ = NULL;
+  this->length_ = 0;
+};
+
+
+template<class T>
+LinkList<T>::LinkList(const T& data) {
+  this->head_ptr_ = new LinkNode<T>(data);
+  this->length_ = 1;
+}
+
+
+template<class T>
+LinkList<T>::LinkList(LinkNode<T>* node_ptr) {
+
+  if (node_ptr == NULL) {
+    LinkNode<T>();
+    return;
+  }
+
+  this->head_ptr_ = node_ptr;
+  this->length_ = 0;
+
+  LinkNode<T>* cur_ptr = head_ptr_;
+
+  while (cur_ptr != NULL) {
+    this->length_++;
+    cur_ptr = cur_ptr->link_;
+  }
+}
+
+
+template<class T>
+LinkList<T>::LinkList(LinkList<T>& link_list) {
+
+  if (link_list.Length() == 0) {
+    head_ptr_ = NULL;
+    length_ = 0;
+    return;
+  }
+
+  LinkNode<T>* cur_src_ptr = link_list.GetHead();
+
+  if (link_list.Length() == 1) {
+    head_ptr_ = new LinkNode<T>(cur_src_ptr->data_);
+    head_ptr_->link_ = NULL;
+    length_ = 1;
+    return;
+  }
+
+  head_ptr_ = new LinkNode<T>();
+
+  LinkNode<T>* cur_dest_ptr = head_ptr_;
+
+  while (cur_src_ptr->link_ != NULL) {
+    cur_dest_ptr->data_ = cur_src_ptr->data_;
+
+    cur_dest_ptr->link_ = new LinkNode<T>();
+
+    cur_dest_ptr = cur_dest_ptr->link_;
+    cur_src_ptr = cur_src_ptr->link_;
+
+    length_++;
+  }
+
+  cur_dest_ptr->data_ = cur_src_ptr->data_;
+  cur_dest_ptr->link_ = NULL;
+}
+
+
+template<class T>
+bool LinkList<T>::GetData(int pos, T& data) const {
+
+  if (pos < 1 || pos > Length()) {
+    return false;
+  }
+
+  LinkNode<T>* cur = head_ptr_;
+
+  while (pos - 1 > 0) {
+    cur = cur->link_;
+    pos--;
+  }
+
+  data = cur->data_;
+
+  return true;
+}
+
+
+template<class T>
+bool LinkList<T>::SetData(int pos, T& data) {
+
+  if (pos < 1 || pos > Length()) {
+    return false;
+  }
+
+  LinkNode<T>* cur = head_ptr_;
+
+  while (pos - 1 > 0) {
+    cur = cur->link_;
+    pos--;
+  }
+
+  cur->data_ = data;
+
+  return true;
+}
+
+
+template<class T>
+void LinkList<T>::MakeEmpty() {
+
+}
+
+
+template<class T>
+void LinkList<T>::Output() {
+
+  if (head_ptr_ == NULL) {
+    cout<<"Empty list"<<endl;
+    return;
+  }
+
+  LinkNode<T>* cur = GetHead();
+  while(cur != NULL) {
+    cout<<cur->data_<<" ";
+    cur = cur->link_;
+  }
+  cout<<endl;
+
+}
+
+
+template<class T>
+bool LinkList<T>::Insert(int pos, LinkNode<T>* node) {
+
+  if (pos < 0 || pos > Length()) {
+    return false;
+  }
+
+  if (node == NULL) {
+    return false;
+  }
+
+  if (head_ptr_ == NULL) {
+    node->link_ = head_ptr_;
+    head_ptr_ = node;
+    length_ = 1;
+    return true;
+  }
+
+  if (pos == 0) {
+
+    node->link_ = head_ptr_;
+    head_ptr_ = node;
+  } else {
+
+    LinkNode<T>* cur = head_ptr_;
+    while (pos - 1 > 0) {
+      cur = cur->link_;
+      pos--;
+    }
+
+    node->link_ = cur->link_;
+    cur->link_ = node;
+  }
+
+  length_++;
+
+  return true;
+}
+
+
+template<class T>
+bool LinkList<T>::Insert(int pos, const T& data) {
+
+  if (pos < 0 || pos > Length()) {
+    return false;
+  }
+
+  LinkNode<T>* node = new LinkNode<T>(data);
+
+  if (head_ptr_ == NULL) {
+    node->link_ = head_ptr_;
+    head_ptr_ = node;
+    length_ = 1;
+    return true;
+  }
+
+  if (pos == 0) {
+
+    node->link_ = head_ptr_;
+    head_ptr_ = node;
+  } else {
+
+    LinkNode<T>* cur = head_ptr_;
+    while (pos - 1 > 0) {
+      cur = cur->link_;
+      pos--;
+    }
+
+    node->link_ = cur->link_;
+    cur->link_ = node;
+  }
+
+  length_++;
+
+  return true;
+}
+
+
+template<class T>
+bool LinkList<T>::IsEmpty() const {
+
+  if (length_ == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+template<class T>
+void LinkList<T>::CyberDashShow() {
+  cout<<endl
+      <<"*************************************** CyberDash ***************************************"<<endl<<endl
+      <<"抖音号\"CyberDash计算机考研\", id: cyberdash_yuan"<<endl<<endl
+      <<"CyberDash成员:"<<endl
+      <<"元哥(cyberdash@163.com), "<<"北京邮电大学(通信工程本科)/北京邮电大学(信息与通信系统研究生)"<<endl
+      <<"磊哥(alei_go@163.com), "<<"山东理工大学(数学本科)/北京邮电大学(计算机研究生)"<<endl<<endl
+      <<"数据结构开源代码(C++清华大学殷人昆)魔改升级版本: https://gitee.com/cyberdash/data-structure-cpp"<<endl
+      <<endl<<"*************************************** CyberDash ***************************************"<<endl<<endl;
+}
 
 
 #endif // CYBER_DASH_LINK_LIST_H
