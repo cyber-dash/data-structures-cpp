13e3ec2d8bb9ffa613d6ea5da2f3d09974f4308a
lizhongyuan <fenixlee@163.com>
2023-03-07 18:22:43 +0800

    静态链表增加若干doxygen

--- LinearList/src/static_linked_list.h
+++ LinearList/src/static_linked_list.h
@@ -17,7 +17,21 @@ using namespace std;
 
 
 /*!
- * @brief 静态链表结点模板结构体
+ * @brief **静态链表结点模板结构体
+ * @note
+ * 静态链表结点模板结构体
+ * --------------------
+ * --------------------
+ * 
+ * <span style="color:#FF8100">
+ * 注意:\n
+ *     当next值为HEAD(0)时, 表示该数组元素是链表最后一个结点\n
+ *     当next值为NONE(-1)时, 表示该数组元素未使用\n
+ * 
+ * 这个设定节省内存, 但如果不清楚则会困惑\n
+ * </span>
+ * 
+ * --------------------
  */
 template <typename TData>
 struct StaticLinkedListNode {
@@ -67,9 +81,9 @@ public:
     static const int HEAD = 0;
 
 private:
-    bool GetInsertIndex_(int& index) const;
+    bool GetInsertionIndex_(int& index) const;
     bool Extend_(int capacity);
-    bool GetNodeIndexByPos_(int pos, int& index) const;
+    bool GetIndexByPos_(int pos, int& index) const;
 
     StaticLinkedListNode<TData>* mem_data_;    //!< 静态链表, mem_data_[0]为头结点(不保存元素)
     int length_;                               //!< 静态链表长度
@@ -90,25 +104,35 @@ private:
  * + **1 初始化成员变量**\n
  * capacity_(容量)使用参数进行初始化\n
  * length_(当前长度)初始化为0\n
- * mem_data_分配内存, capacity_ + 1个元素, 首元素为头结点\n
+ * \n
+ * mem_data_分配内存并初始化, capacity_ + 1个元素, 首元素为头结点\n
  * **if** mem_data_内存分配失败 :\n
  * &emsp; 抛出bad_alloc()异常\n
+ * \n
  * + **2 初始化结点**\n
- * HEAD结点的next指向HEAD(数组索引0)\n
+ * HEAD结点的next指向HEAD<b>(0, 自身)</b>\n
  * **for loop** 数组索引从1到capacity :\n
- * &emsp; 每个数组元素的next指向NONE(索引值-1)\n
+ * &emsp; 当前数组元素的next值为NONE<b>(-1, 表示该数组元素不在静态链表中使用)</b>\n
  */
 template <typename TData>
 StaticLinkedList<TData>::StaticLinkedList(int capacity) {
-    capacity_ = capacity;
-    length_ = 0;
-    mem_data_ = new StaticLinkedListNode<TData>[capacity_ + 1];
-    if (!mem_data_) {
-        throw bad_alloc();
+
+    // ---------- 1 初始化成员变量 ----------
+
+    capacity_ = capacity;                                       // capacity_(容量)使用参数进行初始化
+    length_ = 0;                                                // length_(当前长度)初始化为0
+
+    mem_data_ = new StaticLinkedListNode<TData>[capacity_ + 1]; // mem_data_分配内存并初始化, capacity_ + 1个元素, 首元素为头结点
+    if (!mem_data_) {                                           // if mem_data_内存分配失败
+        throw bad_alloc();                                      // 抛出bad_alloc()异常
     }
 
-    mem_data_[HEAD].next = HEAD;
-    for (int i = 1; i <= capacity; i++) {
+    // ---------- 2 初始化结点 ----------
+
+    mem_data_[HEAD].next = HEAD;                                // HEAD结点的next指向HEAD(0, 自身)
+
+    for (int i = 1; i <= capacity; i++) {                       // for loop 数组索引从1到capacity
+        // 当前数组元素的next值为NONE(-1, 表示该数组元素不在静态链表中使用)
         mem_data_[i].next = StaticLinkedList<TData>::NONE;
     }
 }
@@ -129,7 +153,7 @@ StaticLinkedList<TData>::StaticLinkedList(int capacity) {
  * pos为0时, 取静态链表head结点(数组索引0元素)\n
  * </span>
  * <span style="color:#038575">
- * (pos < 0)或者(pos > 链表长度)时, 返回false\n
+ * (pos < 0)或者(pos > 链表长度)时, 非法位置, 返回false\n
  * </span>
  * <span style="color:#65000b">
  * pos为1时, 取链表第一个元素结点, 与数组的方式有区别\n
@@ -152,7 +176,7 @@ StaticLinkedList<TData>::StaticLinkedList(int capacity) {
  * 返回true\n
  */
 template<typename TData>
-bool StaticLinkedList<TData>::GetNodeIndexByPos_(int pos, int& index) const {
+bool StaticLinkedList<TData>::GetIndexByPos_(int pos, int& index) const {
 
     // ---------- 1 pos非法情况处理 ----------
 
@@ -223,6 +247,52 @@ template<typename TData>
 }
 
 
+/*!
+ * @brief **插入**
+ * @tparam TData 数据项类型模板参数
+ * @param data 待插入数据项
+ * @param pos 插入位置的前一位置
+ * @return 执行结果
+ * @note
+ * 插入
+ * ---
+ * ---
+ * 
+ * <span style="color:#038575">
+ * pos等于0为头结点, 从1开始是数据元素结点 \n
+ * </span>
+ * <span style="color:#FF8100">
+ * 在数组pos位置的后面, 执行插入 \n
+ * </span>
+ * 
+ * ---
+ * + **1 非法位置处理**\n
+ * **if** pos < 0 <b>||</b> pos大于链表长度 :\n
+ * &emsp; 返回false\n
+ * \n
+ * + **2 扩容处理**\n
+ * **if** 链表长度等于容量 :\n
+ * &emsp; 扩容1倍\n
+ * \n
+ * + **3 获取插入位置前一位置的数组索引**\n
+ * 对pos调用GetIndexByPos_, 获取prev_index<b>(插入位置前一位置的数组索引)</b>\n
+ * **if** 获取失败 :\n
+ * &emsp; 返回false\n
+ * \n
+ * + **4 获取插入位置的数组索引**\n
+ * 调用GetInsertionIndex_, 获取insertion_index<b>(执行插入的索引)</b>\n
+ * **if** 获取失败: \n
+ * &emsp; 返回false\n
+ * \n
+ * + **5 执行插入**\n
+ * 插入位置数组元素的next, 指向pos位置数组元素的next\n
+ * pos位置数组元素的next, 指向插入位置\n
+ * 插入位置的data, 等于参数data\n
+ * \n
+ * 链表长度+1\n
+ * + **6 返回结果**\n
+ * 返回true\n
+ */
 template <typename TData>
 bool StaticLinkedList<TData>::Insert(int pos, const TData& data) {
     if (pos < 0 || pos > length_) {
@@ -234,20 +304,20 @@ bool StaticLinkedList<TData>::Insert(int pos, const TData& data) {
     }
 
     int prev_index;
-    bool res = GetNodeIndexByPos_(pos, prev_index);
+    bool res = GetIndexByPos_(pos, prev_index);
     if (!res) {
         return false;
     }
 
-    int insert_index;
-    res = GetInsertIndex_(insert_index);
+    int insertion_index;
+    res = GetInsertionIndex_(insertion_index);
     if (!res) {
         return false;
     }
 
-    mem_data_[insert_index].next = mem_data_[prev_index].next;
-    mem_data_[prev_index].next = insert_index;
-    mem_data_[insert_index].data = data;
+    mem_data_[insertion_index].next = mem_data_[prev_index].next;
+    mem_data_[prev_index].next = insertion_index;
+    mem_data_[insertion_index].data = data;
 
     length_++;
 
@@ -281,6 +351,22 @@ bool StaticLinkedList<TData>::Remove(int pos, TData& data) {
 }
 
 
+/*!
+ * @brief **打印**
+ * @tparam TData 数据项类型模板参数
+ * @note
+ * 打印
+ * ---
+ * ---
+ * 
+ * ---
+ * + **1 空链表处理**\n
+ * + **2 循环打印数据**\n
+ * **for loop** 遍历链表中的每个结点 :\n
+ * &emsp; 打印当前结点的data\n
+ * &emsp; **if** 不是最后一个结点 :\n
+ * &emsp;&emsp; 打印"-->"(表示next指向)\n
+ */
 template<typename TData>
 void StaticLinkedList<TData>::Print() const {
     if (length_ == 0) {
@@ -322,7 +408,7 @@ bool StaticLinkedList<TData>::Extend_(int capacity) {
 
 
 template <typename TData>
-bool StaticLinkedList<TData>::GetInsertIndex_(int& index) const {
+bool StaticLinkedList<TData>::GetInsertionIndex_(int& index) const {
     if (length_ == capacity_) {
         return false;
     }
