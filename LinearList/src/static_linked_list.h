f7dae71b33caa137892f6f8a4e01e420e1e4aa8c
lizhongyuan <fenixlee@163.com>
2023-03-08 17:19:06 +0800

    静态链表完善doxygen, 线性表doxygen配置文件名修改

--- LinearList/src/static_linked_list.h
+++ LinearList/src/static_linked_list.h
@@ -54,38 +54,64 @@ struct StaticLinkedListNode {
  * ------------
  * ------------
  *
+ * <span style="color:#FF8100">
+ * mem_data_[0]为头结点(不保存元素)
+ * </span>
+ *
  * ------------
  */
 template <typename TData>
 class StaticLinkedList {
 public:
+    // 构造函数(容量)
     explicit StaticLinkedList(int capacity = 100);
 
+    /*!
+     * @brief **获取长度**
+     * @return 长度
+     */
     int Length() const { return length_; }
 
+    /*!
+     * @brief **获取容量**
+     * @return 容量
+     */
     int Capacity() const { return capacity_; }
 
+    /*!
+     * @brief **判断是否空链表**
+     * @return 是否空链表
+     */
     bool IsEmpty() const { return length_ == 0; }
 
+    // 搜索
     bool Search(const TData& data, int& pos) const;
 
+    // 插入结点
     bool Insert(int pos, const TData& data);
 
+    // 删除结点
     bool Remove(int pos, TData& data);
 
+    // 打印
     void Print() const;
 
     StaticLinkedListNode<TData>& operator[] (size_t index);
 
-    static const int NONE = -1;
-    static const int HEAD = 0;
+    static const int NONE = -1;                //!< 表示该数组元素不在静态链表中使用
+    static const int HEAD = 0;                 //!< 头结点的数组索引 
 
 private:
+    // 获取插入位置的数组索引
     bool GetInsertionIndex_(int& index) const;
+
+    // 扩容
     bool Extend_(int increased_capacity);
+
+    // 根据pos获取结点的数组索引
     bool GetIndexByPos_(int pos, int& index) const;
 
-    StaticLinkedListNode<TData>* mem_data_;    //!< 静态链表, mem_data_[0]为头结点(不保存元素)
+    StaticLinkedListNode<TData>* mem_data_;    //!< 静态链表数组
     int length_;                               //!< 静态链表长度
     int capacity_;                             //!< 静态链表容量
 };
@@ -132,8 +158,7 @@ StaticLinkedList<TData>::StaticLinkedList(int capacity) {
     mem_data_[HEAD].next = HEAD;                                // HEAD结点的next指向HEAD(0, 自身)
 
     for (int i = 1; i <= capacity; i++) {                       // for loop 数组索引从1到capacity
-        // 当前数组元素的next值为NONE(-1, 表示该数组元素不在静态链表中使用)
-        mem_data_[i].next = StaticLinkedList<TData>::NONE;
+        mem_data_[i].next = StaticLinkedList<TData>::NONE;      // 当前数组元素的next值为NONE(-1, 表示该数组元素不在静态链表中使用)
     }
 }
 
@@ -221,12 +246,15 @@ bool StaticLinkedList<TData>::GetIndexByPos_(int pos, int& index) const {
  * + **1 空链表处理**\n
  * **if** 空链表 :\n
  * &emsp; 返回false\n
+ * \n
  * + **2 遍历链表搜索**\n
  * **for loop** 遍历链表的每个结点 :\n
  * &emsp; **if** 当前结点的data等于参数data :\n
  * &emsp;&emsp; cur_pos(当前位置)赋给pos\n
  * &emsp;&emsp; 返回true\n
+ * \n
  * &emsp; cur_pos加1\n
+ * \n
  * + **3 返回false**\n
  */
 template<typename TData>
@@ -258,15 +286,15 @@ bool StaticLinkedList<TData>::Search(const TData& data, int& pos) const {
 
 
 /*!
- * @brief **插入**
+ * @brief **插入结点**
  * @tparam TData 数据项类型模板参数
  * @param data 待插入数据项
  * @param pos 插入位置的前一位置
  * @return 执行结果
  * @note
- * 插入
- * ---
- * ---
+ * 插入结点
+ * -------
+ * -------
  * 
  * <span style="color:#038575">
  * pos等于0为头结点, 从1开始是数据元素结点 \n
@@ -275,7 +303,7 @@ bool StaticLinkedList<TData>::Search(const TData& data, int& pos) const {
  * 在数组pos位置的后面, 执行插入 \n
  * </span>
  * 
- * ---
+ * -------
  * + **1 非法位置处理**\n
  * **if** pos < 0 <b>||</b> pos大于链表长度 :\n
  * &emsp; 返回false\n
@@ -305,33 +333,45 @@ bool StaticLinkedList<TData>::Search(const TData& data, int& pos) const {
  */
 template <typename TData>
 bool StaticLinkedList<TData>::Insert(int pos, const TData& data) {
-    if (pos < 0 || pos > length_) {
-        return false;
+
+    // ---------- 1 非法位置处理 ----------
+
+    if (pos < 0 || pos > length_) {                                 // if pos < 0 || pos大于链表长度
+        return false;                                               // 返回false
     }
 
-    if (length_ == capacity_) {
-        this->Extend_(capacity_);
+    // ---------- 2 扩容处理 ----------
+
+    if (length_ == capacity_) {                                     // if 链表长度等于容量
+        this->Extend_(capacity_);                                   // 扩容1倍
     }
 
+    // ---------- 3 获取插入位置前一位置的数组索引 ----------
     int prev_index;
-    bool res = GetIndexByPos_(pos, prev_index);
-    if (!res) {
-        return false;
+    bool res = GetIndexByPos_(pos, prev_index);                     // 对pos调用GetIndexByPos_, 获取prev_index(插入位置前一位置的数组索引)
+    if (!res) {                                                     // if 获取失败
+        return false;                                               // 返回false
     }
+
+    // ---------- 4 获取插入位置的数组索引 ----------
 
     int insertion_index;
-    res = GetInsertionIndex_(insertion_index);
-    if (!res) {
-        return false;
+    res = GetInsertionIndex_(insertion_index);                      // 调用GetInsertionIndex_, 获取insertion_index(执行插入的索引)
+    if (!res) {                                                     // if 获取失败
+        return false;                                               // 返回false
     }
 
-    mem_data_[insertion_index].next = mem_data_[prev_index].next;
-    mem_data_[prev_index].next = insertion_index;
-    mem_data_[insertion_index].data = data;
+    // ---------- 5 执行插入 ----------
 
-    length_++;
+    mem_data_[insertion_index].next = mem_data_[prev_index].next;   // 插入位置数组元素的next, 指向pos位置数组元素的next
+    mem_data_[prev_index].next = insertion_index;                   // pos位置数组元素的next, 指向插入位置
+    mem_data_[insertion_index].data = data;                         // 插入位置的data, 等于参数data
 
-    return true;
+    length_++;                                                      // 链表长度+1
+
+    // ---------- 6 返回结果 ----------
+
+    return true;                                                    // 返回true
 }
 
 
@@ -350,47 +390,59 @@ bool StaticLinkedList<TData>::Insert(int pos, const TData& data) {
  * + **1 非法情况处理**\n
  * **if** 空链表 :\n
  * &emsp; 返回false\n
+ * \n
  * **if** pos < 1 <b>||</b> pos > 链表长度 :\n
  * &emsp; 返回false\n
+ * \n
  * + **2 找到删除结点前一结点**\n
  * 初始化prev_index(待删除结点前一结点的数组索引)\n
  * **for loop** 从位置1遍历到位置pos - 1 :\n
  * &emsp; prev_index值改为mem_data_[prev_index].next\n
+ * \n
  * + **3 删除**\n
  * 初始化deletion_index(待删除结点的数组索引), 等于mem_data_[prev_index].next\n
  * 前一结点的next, 指向待删除结点的next\n
  * 待删除结点的next设置为NONE(此数组元素不再在链表中)\n
  * 被删除结点的data赋给参数data\n
  * 链表长度减1\n
+ * \n
  * + **4 退出函数**\n
  * 返回true\n
  */
 template<typename TData>
 bool StaticLinkedList<TData>::Remove(int pos, TData& data) {
 
-    if (length_ == 0) {
-        return false;
+    // ---------- 1 非法情况处理 ----------
+
+    if (length_ == 0) {                                             // if 空链表
+        return false;                                               // 返回false
     }
 
-    if (pos < 1 || pos > length_) {
-        return false;
+    if (pos < 1 || pos > length_) {                                 // if pos < 1 || pos > 链表长度
+        return false;                                               // 返回false
     }
 
-    int prev_index = HEAD;
-    for (int i = 1; i < pos; i++) {
-        prev_index = mem_data_[prev_index].next;
+    // ---------- 2 找到删除结点前一结点 ----------
+
+    int prev_index = HEAD;                                          // 初始化prev_index(待删除结点前一结点的数组索引)
+    for (int i = 1; i < pos; i++) {                                 // for loop 从位置1遍历到位置pos - 1
+        prev_index = mem_data_[prev_index].next;                    // prev_index值改为mem_data_[prev_index].next
     }
 
-    int deletion_index = mem_data_[prev_index].next;
-    mem_data_[prev_index].next = mem_data_[deletion_index].next;
+    // ---------- 3 删除 ----------
 
-    mem_data_[deletion_index].next = NONE;
+    int deletion_index = mem_data_[prev_index].next;                // 初始化deletion_index(待删除结点的数组索引), 等于mem_data_[prev_index].next
+    mem_data_[prev_index].next = mem_data_[deletion_index].next;    // 前一结点的next, 指向待删除结点的next
 
-    data = mem_data_[deletion_index].data;
+    mem_data_[deletion_index].next = NONE;                          // 待删除结点的next设置为NONE(此数组元素不再在链表中)
 
-    length_--;
+    data = mem_data_[deletion_index].data;                          // 被删除结点的data赋给参数data
 
-    return true;
+    length_--;                                                      // 链表长度减1
+
+    // ---------- 4 退出函数 ----------
+
+    return true;                                                    // 返回true
 }
 
 
@@ -404,6 +456,9 @@ bool StaticLinkedList<TData>::Remove(int pos, TData& data) {
  * 
  * ---
  * + **1 空链表处理**\n
+ * **if** 空链表 :\n
+ * &emsp; 打印"Empty list"\n
+ * \n
  * + **2 循环打印数据**\n
  * **for loop** 遍历链表中的每个结点 :\n
  * &emsp; 打印当前结点的data\n
@@ -412,14 +467,19 @@ bool StaticLinkedList<TData>::Remove(int pos, TData& data) {
  */
 template<typename TData>
 void StaticLinkedList<TData>::Print() const {
-    if (length_ == 0) {
-        cout << "Empty list" << endl;
+
+    // ---------- 1 空链表处理 ----------
+
+    if (length_ == 0) {                                                             // if 空链表
+        cout << "Empty list" << endl;                                               // 打印"Empty list"
     }
 
-    for (int cur = mem_data_[HEAD].next; cur != HEAD; cur = mem_data_[cur].next) {
-        cout << mem_data_[cur].data;
-        if (mem_data_[cur].next != HEAD) {
-            cout << "-->";
+    // ---------- 2 循环打印数据 ----------
+
+    for (int cur = mem_data_[HEAD].next; cur != HEAD; cur = mem_data_[cur].next) {  // for loop 遍历链表中的每个结点
+        cout << mem_data_[cur].data;                                                // 打印当前结点的data
+        if (mem_data_[cur].next != HEAD) {                                          // if 不是最后一个结点
+            cout << "-->";                                                          // 打印"-->"(表示next指向)
         }
     }
 
@@ -441,42 +501,60 @@ void StaticLinkedList<TData>::Print() const {
  * + **1 容量数值增加**\n
  * 使用old_capacity保存旧链表的容量\n
  * capacity_增加数值\n
+ * \n
  * + **2 开辟新内存**\n
  * 使用新容量, 初始化并分配内存\n
  * **if** 新内存分配失败 :\n
  * &emsp; 返回false\n
+ * \n
  * + **3 新内存赋值**\n
  * **for loop** 遍历原链表容量内的数组元素\n
  * &emsp; new_mem_data[i] = mem_data_[i]\n
+ * \n
  * **for loop** 遍历新链表新增容量的数组元素\n
  * &emsp; 每个元素的next设置为NONE(-1, 不在链表中)\n
+ * \n
  * + **4 使用新内存并释放旧内存**\n
  * 释放旧内存\n
  * 指向新内存\n
+ * \n
  * + **5 退出函数**\n
  * 返回true\n
  */
 template <typename TData>
 bool StaticLinkedList<TData>::Extend_(int increased_capacity) {
-    int old_capacity = capacity_;
-    capacity_ += increased_capacity;
+
+    // ----------1 容量数值增加 ----------
+
+    int old_capacity = capacity_;                           // 使用old_capacity保存旧链表的容量
+    capacity_ += increased_capacity;                        // capacity_增加数值
+
+    // ---------- 2 开辟新内存 ----------
+
+    // 使用新容量, 初始化并分配内存
     StaticLinkedListNode<TData>* new_mem_data = new StaticLinkedListNode<TData>[capacity_ + 1];
-    if (!new_mem_data) {
-        return false;
+    if (!new_mem_data) {                                    // if 新内存分配失败
+        return false;                                       // 返回false
     }
 
-    for (int i = HEAD; i <= old_capacity; i++) {
-        new_mem_data[i] = mem_data_[i];
+    // ---------- 3 新内存赋值 ----------
+
+    for (int i = HEAD; i <= old_capacity; i++) {            // for loop 遍历原链表容量内的数组元素
+        new_mem_data[i] = mem_data_[i];                     // new_mem_data[i] = mem_data_[i]
     }
 
-    for (int i = old_capacity + 1; i <= capacity_; i++) {
-        new_mem_data[i].next = NONE;
+    for (int i = old_capacity + 1; i <= capacity_; i++) {   // for loop 遍历新链表新增容量的数组元素
+        new_mem_data[i].next = NONE;                        // 每个元素的next设置为NONE(-1, 不在链表中)
     }
 
-    delete[] mem_data_;
-    mem_data_ = new_mem_data;
+    // ---------- 4 使用新内存并释放旧内存 ----------
 
-    return true;
+    delete[] mem_data_;                                     // 释放旧内存
+    mem_data_ = new_mem_data;                               // 指向新内存
+
+    // ---------- 5 退出函数 ----------
+
+    return true;                                            // 返回true
 }
 
 
@@ -494,11 +572,13 @@ bool StaticLinkedList<TData>::Extend_(int increased_capacity) {
  * + **1 非法情况处理**\n
  * **if** 链表长度等于容量 :\n
  * &emsp; 返回false\n
+ * \n
  * + **2 查找到插入位置并赋值**\n
  * **for loop** 从位置1遍历到位置length_ + 1 :\n
  * &emsp; **if** 当前位置的next等于NONE(-1, 未被链表使用) :\n
  * &emsp;&emsp; 当前位置的索引i, 赋给参数index\n
  * &emsp;&emsp; 返回true\n
+ * \n
  * + **3 退出函数**\n
  * 返回false(失败情况处理)\n
  */
