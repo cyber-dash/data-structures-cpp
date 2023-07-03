657b5a10ee9fc0372a714fd16264e9b1036cb86a
lizhongyuan <fenixlee@163.com>
2023-07-03 11:55:58 +0800

    排序增加readme代码指向

--- Sort/src/shell_sort.h
+++ Sort/src/shell_sort.h
@@ -1,6 +1,27 @@
+/*!
+ * @file shell_sort.h
+ * @author CyberDash计算机考研, cyberdash@163.com(抖音id:cyberdash_yuan)
+ * @brief 希尔排序
+ * @version 0.2.1
+ * @date 2021-09-19
+ */
 
+
+/*!
+ * @brief **间隔区间插入排序**
+ * @tparam TElement 数组元素类型模板参数
+ * @param elements 数组
+ * @param gap 间隔
+ * @param length 数组长度
+ * @note
+ * 间隔区间插入排序
+ * -------------
+ * -------------
+ *
+ * -------------
+ */
 template <typename TElement>
-void GappedInsertionSort(TElement* elements, int gap, int length) {
+void GapedInsertionSort(TElement* elements, int gap, int length) {
     for (int i = gap; i < length; i++) {
 
         if (elements[i] >= elements[i - gap]) {
@@ -18,9 +39,23 @@ void GappedInsertionSort(TElement* elements, int gap, int length) {
 }
 
 
+/*!
+ * @brief **希尔排序**
+ * @tparam TElement
+ * @param elements 数组
+ * @param gaps 间隔数组
+ * @param length 数组长度
+ * @param gaps_count 间隔数组长度
+ * @note
+ * 希尔排序
+ * -------
+ * -------
+ *
+ * -------
+ */
 template <typename TElement>
 void ShellSort(TElement* elements, int* gaps, int length, int gaps_count) {
     for (int i = 0; i < gaps_count; i++) {
-        GappedInsertionSort(elements, gaps[i], length);
+        GapedInsertionSort(elements, gaps[i], length);
     }
 }
